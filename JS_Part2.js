42963c68e46a9f1c5b938f2ac745f32506960e9f
Gabriella Luck <gabbyluck@hotmail.co.uk>
2021-06-28 21:44:19 +0100

    Part 2

---
+++ JS_Part2.js
@@ -0,0 +1,44 @@
+/* What are the eight data types in JavaScript?
+ Number : integers/floating point/'special numeric values'(infinity, -infinity, NaN).
+ BigInt : values larger than 2⁵³-1 or smaller than -2⁵³-1. Created by appending 'n' to the end of an integer. String : "" double quotes '' single quotes `` back ticks. 
+ Boolean : True or False.
+ Null : Nothing, empty or unknown.
+ Undefined : No assigned value.
+ Object & Sysmbol : Object stores collections of data and more complex entities. Symbol creates unique identifiers for objects.
+ Type of : Returns the type of the argument. 
+
+ Which data type is NOT primitive? Object 
+
+ What is the relationship between null and undefined?
+ Null will become 0 Undefined becomes NaN
+
+ What is the difference between single, double, and backtick quotes for strings?
+ There is no difference between single and double but they shouldn't be mixed or the browser will not recognise the string.
+ Backticks create 'template literals'.
+
+
+
+
+
+
+
+
+
+
+
+
+What is the term for embedding variables/expressions in a string?
+Which type of quote lets you embed variables/expressions in a string?
+How do you embed variables/expressions in a string?
+How do you escape characters in a string?
+What are methods?
+What is the difference between slice/substring/substr?
+What are the three logical operators and what do they stand for?
+What are the comparison operators?
+What are truthy and falsy values?
+What are the falsy values in JavaScript?
+What are conditionals?
+What is the syntax for an if/else conditional?
+What is the syntax for a switch statement?
+What is the syntax for a ternary operator?
+What is nesting?*/
